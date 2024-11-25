7b295475a132248d32d5927bf9b6527fa8d5885d
Yicheng D <yxd245@case.edu>
2024-11-25 17:08:19 -0500

    add test main

---
+++ test_main.py
@@ -0,0 +1,6 @@
+import os
+
+video_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test/data/2023-01-01_1800_US_CNN_CNN_Newsroom_With_Fredricka_Whitfield.mp4.mp4")
+CC_json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test/data/2023-01-01_1800_US_CNN_CNN_Newsroom_With_Fredricka_Whitfield.json")
+
+
