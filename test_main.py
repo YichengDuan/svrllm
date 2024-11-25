214e83a9dfb3b2c9ec6da65ff346f2e204dd7dbe
Yicheng D <yxd245@case.edu>
2024-11-25 17:32:08 -0500

    pp update

--- test_main.py
+++ test_main.py
@@ -4,3 +4,4 @@ video_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test/data/
 CC_json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test/data/2023-01-01_1800_US_CNN_CNN_Newsroom_With_Fredricka_Whitfield.json")
 
 
+# 实现 从 。/services 里调取 视频处理模块
